99e9dba78b9efac0084fa1c0f5c06c652ae177d4
vinicius-silva-compasso <104840662+vinicius-silva-compasso@users.noreply.github.com>
2025-06-23 20:25:36 +0000

    Adiciona lista de inscritos nas atividades

--- src/static/app.js
+++ src/static/app.js
@@ -25,6 +25,10 @@ document.addEventListener("DOMContentLoaded", () => {
           <p>${details.description}</p>
           <p><strong>Schedule:</strong> ${details.schedule}</p>
           <p><strong>Availability:</strong> ${spotsLeft} spots left</p>
+          <p><strong>Inscritos:</strong> ${details.participants.length > 0
+            ? details.participants.join(", ")
+            : '<em>Nenhum inscrito ainda</em>'}
+          </p>
         `;
 
         activitiesList.appendChild(activityCard);
